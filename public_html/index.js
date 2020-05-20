0dadeb7ded6ba24185e0d4de427ca83f06f448c3
NicoZerba <zerbato.nicolo@gmail.com>
2020-05-20 17:37:42 +0200

    add index.js and modify index.html

---
+++ public_html/index.js
@@ -0,0 +1,40 @@
+
+const API_URL = "http://localhost:8080/posts";
+window.onload = () => {
+    getPosts();
+}
+
+const getPosts = () => {
+    fetch(API_URL, {
+        method: 'GET'
+    }).then((response) => {
+        return response.json();
+    }).then((data) => {
+        buildPosts(data);
+    });
+}
+
+const buildPosts = (blogPosts) => {
+    let blogPostsContent = "";
+    for(var blogPost of blogPosts) {
+        const postDate = new Date(parseInt(blogPost.added_date)).toDateString();
+        const postLink = `/post.html?id=${blogPost.id}`;
+
+        blogPostsContent += `
+            <a class="post-link" href="${postLink}">
+                <div class="post">
+                    <div class="post-content">
+                        <div class="post-date">${postDate}</div>
+                        <div class="post-title">
+                            <h4>${blogPost.title}</h4>
+                        </div>
+                        <div class="post-text">
+                            <p>${blogPost.content}</p>
+                        </div>
+                    </div>
+                </div>
+            </a>
+        `;
+    }
+    document.querySelector(".blog-posts").innerHTML = blogPostsContent;
+}
